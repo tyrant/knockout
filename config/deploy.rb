7fe14dcc08c40d123b953cfcbef00315d1313c55
Mikey Clarke <epicschnozz@gmail.com>
2017-12-08 00:03:28 +1300

    Tweaked deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.10.0"
 
-set :application, "galleon"
-set :repo_url, "git@github.com:tyrant/galleon.git"
+set :application, "knockout"
+set :repo_url, "git@github.com:tyrant/knockout.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/home/app-user/galleon"
+set :deploy_to, "/home/app-user/knockout"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
